31d6c776a0cb21c57633f76b1c6fe00bfe9ed1fe
LucasDevSystem <highspeedsetup@gmail.com>
2023-08-03 15:18:44 -0300

    Fix (Money) deleted money and dolar

--- src/test/java/com/tdd/demo/MoneyTest.java
+++ src/test/java/com/tdd/demo/MoneyTest.java
@@ -11,15 +11,18 @@ public class MoneyTest {
     public void testEquality() {
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(5)));
-     }
-     
+    }
+
     @Test
     public void testMultiplication() {
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
     }
+
+    public void testDifferentClassEquality() {
+        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
+    }
+
 }
